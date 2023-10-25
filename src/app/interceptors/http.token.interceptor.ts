8c00719533bff682465dfa9cd965b9de1d1e955f
wesley mbate <wmbate@meliora.tech>
2023-10-25 10:02:28 +0300

    docs ready for deployment

--- src/app/interceptors/http.token.interceptor.ts
+++ src/app/interceptors/http.token.interceptor.ts
@@ -22,7 +22,7 @@ export class HttpTokenInterceptor implements HttpInterceptor {
       return next.handle(req);
     }
 
-    const token = '8b2426118581b68d8íc1bc95987c4e8a';
+    const token = 'f3aba333c07b589a5449cc5868c10be1';
 
     const request = req.clone({
       setHeaders: {
