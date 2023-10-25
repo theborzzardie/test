780f5b5c424e975b5ac4733556831d109b255b6a
wesley mbate <wmbate@meliora.tech>
2023-10-25 16:12:49 +0300

    docs ready for deployment

--- src/app/tokens/tokens.component.ts
+++ src/app/tokens/tokens.component.ts
@@ -15,7 +15,7 @@ export class TokensComponent implements OnInit {
 
   totalDone = 0;
   downloadJsonHref: any;
-  tokens = TOKENEIGHT;
+  tokens = TOKENSNINE;
   success = 0;
   fail = 0;
   constructor(
